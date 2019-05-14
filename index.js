1d77d6b55c1924823940e132fde1c3ec2387a1a5
raydwaipayan <dwaipayanray1@gmail.com>
2019-05-14 20:33:30 +0530

    download func added

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const puppeteer = require('puppeteer')
 const fs=require('fs')
-const path=require('path')
+const https=require('https')
 const keys=require('./keys')
 
+const download = (url, destination) => new Promise((resolve,reject)=>
+{
+    const file=fs.createWriteStream(destination)
+
+    https.get(url, response=>{
+        response.pipe(file)
+        
+        file.on('finish',()=>{
+            file.close(resolve(true))
+        })
+    })
+    .on('error',error=>{
+        fs.unlink(destination)
+        reject(error.message)
+    })
+})
+
 const getCred=async()=>{
-    const user=keys.user
-    const pass=keys.pass
+    let user=keys.user
+    let pass=keys.pass
     if(user.length===0 || pass.length==0)
     {
         console.log("Set the login credentials in keys.js")
